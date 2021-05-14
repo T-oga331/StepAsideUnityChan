9ffbe6d1fa62cc973f657470ece289378338c0b0
T-oga331 <81658428+T-oga331@users.noreply.github.com>
2021-05-14 23:31:13 +0900

    Initial commit

---
+++ Assets/DeleteTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteTest : MonoBehaviour
+{
+    // カメラに写っていないときにオブジェクトを削除
+    void OnBecameInvisible()
+    {
+        GameObject.Destroy(this.gameObject);
+    }
+
+}
---
+++ Assets/MyCameraController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCameraController : MonoBehaviour
+{
+    //Unityちゃんのオブジェクト
+    private GameObject unitychan;
+    //Unityちゃんとカメラの距離
+    private float difference;
+    //コインのオブジェクト（追加）
+    public GameObject CoinPrefab;
+    //車のオブジェクト（追加）
+    public GameObject CarPrefab;
+    //コーンのオブジェクト（追加）
+    public GameObject TrafficConePrefab;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+        //Unityちゃんとカメラの位置（z座標）の差を求める
+        this.difference = unitychan.transform.position.z - this.transform.position.z;
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Unityちゃんの位置に合わせてカメラの位置を移動
+        this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z - difference);
+    }
+
+    void OnBecameInvisible()
+    {
+        //コインを削除（追加）
+        GameObject.Destroy(CoinPrefab);
+        //車を削除（追加）
+        GameObject.Destroy(CarPrefab);
+        //コーンを削除（追加）
+        GameObject.Destroy(TrafficConePrefab);
+    }
+
+
+}
